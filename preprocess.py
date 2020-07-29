d9d361da7d0a293f1a5821c4fd6ce5cf2f0126b7
Kuuuna98 <yunaghgh@naver.com>
2020-07-29 10:04:44 +0900

    Modify preprocess code

--- preprocess.py
+++ preprocess.py
@@ -11,29 +11,25 @@ def get_factor(image):
 
 
 def random_resize(image, bboxes):
-
     last_coord = get_factor(image)
-
     random_diff = random_factor(-0.3 * last_coord, 0.3 * last_coord)
-
     random_diff_ratio = random_diff / last_coord
-
-    do_a_flip_random = random_factor()
+    do_a_resize_random = random_factor()
 
     adjusted_image = tf.cond(
-        do_a_flip_random < 0.3,
-        true_fn=lambda: image_resize(image, random_diff_ratio),
+        do_a_resize_random < 0.3,
+        true_fn=lambda: image_resize(image,random_diff_ratio),
         false_fn=lambda: image)
 
     adjusted_bboxes = tf.cond(
-        do_a_flip_random < 0.3,
-        true_fn=lambda: bbox_resize(image, bboxes, random_diff, random_diff_ratio),
+        do_a_resize_random < 0.3,
+        true_fn=lambda: bbox_resize(last_coord, bboxes, random_diff),
         false_fn=lambda: bboxes)
 
     return adjusted_image, adjusted_bboxes
 
 
-def image_resize(image, random_diff_ratio):
+def image_resize(image,random_diff_ratio):
     input_size = tf.cast(tf.shape(image)[0], tf.float32)
     resized_image = tf.image.crop_and_resize([image],
                                              [[random_diff_ratio, random_diff_ratio, 1 - random_diff_ratio,
@@ -43,14 +39,11 @@ def image_resize(image, random_diff_ratio):
     return resized_image
 
 
-def bbox_resize(image, bboxes, random_diff, random_diff_ratio):
-    last_coord = get_factor(image)
-
+def bbox_resize(last_coord, bboxes, random_diff):
     valid_bboxes = bboxes[tf.reduce_any(bboxes != 0, axis=-1)]
-
     resized_bboxes = (valid_bboxes - random_diff) * last_coord / (last_coord - 2 * random_diff)
 
-    if random_diff_ratio > 0: resized_bboxes = crop_bbox(image, resized_bboxes)
+    if random_diff > 0: resized_bboxes = crop_bbox(last_coord, resized_bboxes)
 
     resized_bboxes = tf.pad(resized_bboxes, [[0, tf.shape(bboxes)[0] - tf.shape(resized_bboxes)[0]], [0, 0]],
                             'CONSTANT')
@@ -58,11 +51,9 @@ def bbox_resize(image, bboxes, random_diff, random_diff_ratio):
     return resized_bboxes
 
 
-def crop_bbox(image, bboxes):
-    last_coord = get_factor(image)
-
+def crop_bbox(last_coord, bboxes):
     uncropped_bboxes = bboxes[tf.reduce_all(tf.logical_and((bboxes > 0), (bboxes < last_coord)), axis=-1)]
-    cropped_bboxes = bboxes[tf.reduce_any(
+    cropped_bboxes = bboxes[tf.reduce_all(
         tf.logical_and(tf.logical_and((bboxes[:, 0::4] < last_coord), (bboxes[:, 2::4] > 0)),
                        tf.logical_and((bboxes[:, 1::4] < last_coord), (bboxes[:, 3::4] > 0))), axis=-1)]
 
@@ -100,12 +91,8 @@ def image_flip_lr(image):
 
 def bbox_flip_lr(image, bboxes):
     last_coord = get_factor(image)
-
     valid_bboxes = bboxes[tf.reduce_any(bboxes != 0, axis=-1)]
-
     flipped_bboxes = tf.gather(valid_bboxes, [2, 1, 0, 3], axis=-1) * [-1., 1., -1., 1.] + [last_coord, 0., last_coord, 0.]
-
-    flipped_bboxes = tf.pad(flipped_bboxes, [[0, tf.shape(bboxes)[0] - tf.shape(flipped_bboxes)[0]], [0, 0]],
-                            'CONSTANT')
+    flipped_bboxes = tf.pad(flipped_bboxes, [[0, tf.shape(bboxes)[0] - tf.shape(flipped_bboxes)[0]], [0, 0]],'CONSTANT')
 
     return flipped_bboxes
